3b4f3536036166f5523ea3ec5a6dc00598faeb09
tenkoff <tenkoff757513@gmail.com>
2022-03-24 23:45:15 +0700

    completed 5 tasks in the 2nd lesson

---
+++ lesson2/car.js
@@ -0,0 +1,18 @@
+const carName = 'mazda';
+let maxCarSpeed = '320';
+let carOwner = 'Slavka';
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+maxCarSpeed = '280';
+console.log(maxCarSpeed);
+maxCarSpeed = '240';
+console.log(maxCarSpeed);
+maxCarSpeed = '190';
+console.log(maxCarSpeed);
+carOwner = 'Slava';
+console.log(carOwner);
+carOwner = 'Slaventy';
+console.log(carOwner);
+carOwner = 'Vyacheslave';
+console.log(carOwner);
---
+++ lesson2/script.js
@@ -0,0 +1,13 @@
+// JavaScript код с созданием переменных\
+/*Автор
+Теньковский Вячеслав*/
+let myName = 'Вячеслав';
+const mySurname = 'Теньковский';
+let myFavoriteDrink = 'Пивас';
+let myFavoriteAnimal = 'Любимое животное Степан';
+let myFavoriteProgrammingLanguage = ' Любимый фраемворк Ларка';
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
