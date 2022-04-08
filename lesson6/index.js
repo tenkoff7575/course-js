334635cbaa737751752acbc5007f777ece757117
tenkoff <tenkoff757513@gmail.com>
2022-04-08 21:06:04 +0700

    lesson 6 task 2 fixed

--- lesson6/index.js
+++ lesson6/index.js
@@ -14,29 +14,26 @@ if (userLogin === existedUserLogin && userPassword === existedUserPassword) {
 
 for (let i = 0; i < 3; i += 1) {
     let newStudent = prompt('Введите имя нового студента!').trim();
-
-if (newStudent) {
-    alert(`Добро пожаловать, ${newStudent}!`);
+    if (newStudent) {
+        alert(`Добро пожаловать, ${newStudent}!`);
     }
  };
 
 i = 0;
  while (i < 3) {
     let newStudent = prompt('Введите имя нового студента!').trim();
-
-if (newStudent) {
-    alert(`Добро пожаловать, ${newStudent}!`);
-    i += 1;
+    if (newStudent) {
+        alert(`Добро пожаловать, ${newStudent}!`);
+        i += 1;
     }
 };
 
 i = 0;
 do {
     let newStudent = prompt('Введите имя нового студента!').trim();
-
-if (newStudent) {
-    alert(`Добро пожаловать, ${newStudent}!`);
-    i += 1;
+    if (newStudent) {
+        alert(`Добро пожаловать, ${newStudent}!`);
+        i += 1;
     }
 }
 while (i < 3);
